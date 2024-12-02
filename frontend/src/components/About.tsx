3a8531dd39dfff6ad95042b0a256166a159914ab
Farhaan111 <114086902+Farhaan111@users.noreply.github.com>
2024-12-02 21:53:55 +0530

    about page and welcome styling

--- frontend/src/components/About.tsx
+++ frontend/src/components/About.tsx
@@ -1,5 +1,15 @@
+import "./About.css";
 const About = () => {
-  return <div>My about</div>;
+  return <div className="about-page"> 
+  <img
+  src="/images/inventorybg.png"
+  alt="Inventory Background"
+  className="bg-image"
+/>
+<div >
+  <h1 className="trackify-text">trackify</h1>
+  <p className="about-content"> is designed to be a better inventory experience, offering businesses a seamless and efficient way to manage their stock, transactions, and suppliers. With an intuitive interface and robust features, it simplifies the process of tracking items, monitoring stock levels, and ensuring accurate record-keeping. Whether you’re handling inbound goods from suppliers, in-house production, or outbound sales to customers, our system provides real-time insights and detailed reporting to keep your operations running smoothly. Built with advanced technology and a user-focused design, it’s perfect for businesses looking to enhance productivity, reduce waste, and gain control over their inventory. Experience streamlined inventory management like never before!</p>
+</div></div>;
 };
 
 export default About;
