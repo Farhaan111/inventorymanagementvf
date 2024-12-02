4aa15b061a06bd06e3eb2942ffd4f0cac25011d1
Farhaan111 <114086902+Farhaan111@users.noreply.github.com>
2024-12-02 23:21:23 +0530

    added about and font

--- frontend/src/components/About.tsx
+++ frontend/src/components/About.tsx
@@ -6,8 +6,8 @@ const About = () => {
   alt="Inventory Background"
   className="bg-image"
 />
-<div >
-  <h1 className="trackify-text">trackify</h1>
+<div className="about-container" >
+  <h1 className="trackify-text">trackify.org</h1>
   <p className="about-content"> is designed to be a better inventory experience, offering businesses a seamless and efficient way to manage their stock, transactions, and suppliers. With an intuitive interface and robust features, it simplifies the process of tracking items, monitoring stock levels, and ensuring accurate record-keeping. Whether you’re handling inbound goods from suppliers, in-house production, or outbound sales to customers, our system provides real-time insights and detailed reporting to keep your operations running smoothly. Built with advanced technology and a user-focused design, it’s perfect for businesses looking to enhance productivity, reduce waste, and gain control over their inventory. Experience streamlined inventory management like never before!</p>
 </div></div>;
 };
